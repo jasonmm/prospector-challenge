78ff979e24175622b9c65eabf67f724acf2abd21
Jason <jasonmm.github@gmail.com>
2017-12-24 17:49:03 -0600

    Moved code back into bot namespace file.

--- src/main/clojure/xyz/jmullin/prospector/bot/JasonBot.clj
+++ src/main/clojure/xyz/jmullin/prospector/bot/JasonBot.clj
@@ -1,8 +1,48 @@
 (ns xyz.jmullin.prospector.bot.JasonBot
   ""
   (:gen-class :implements [xyz.jmullin.prospector.game.ProspectorBot])
-  (:require [xyz.jmullin.prospector.bot.jasonbot-algos.JasonBotDivideAndConquer :as dac]))
+  (:require [clojure.pprint :as pp]))
 
+(defn probe-points
+  [[x y] dimension]
+  (let [dim-quarter (quot dimension 4)
+        point1 [(- x dim-quarter) (- y dim-quarter)]
+        point2 [(+ x dim-quarter) (- y dim-quarter)]
+        point3 [(+ x dim-quarter) (+ y dim-quarter)]
+        point4 [(- x dim-quarter) (+ y dim-quarter)]]
+    [[x y] point1 point2 point3 point4]))
+
+(defn coord [x y] (new xyz.jmullin.prospector.game.Coord x y))
+(defn probe-fn [probe] (fn [x y] (.query probe (coord x y))))
+
+(defn divide-and-conquer
+  "Prospects by dividing a plot centered at `center` into four sections, probing `center` and the center of each section, and using the largest value as the center of a plot 1/4th the size of the current plot."
+  [[center-x center-y] dimension probe]
+  (let [probe        (probe-fn probe)
+        probe-values []]
+    (loop [[center-x center-y] [center-x center-y]
+           dimension           dimension
+           probe-round         1]
+      (let [points-to-probe (probe-points [center-x center-y] dimension)
+            probe-values    (->> points-to-probe
+                                 (map (fn [[x y]] {[x y] (probe x y)}))
+                                 (concat probe-values)
+                                 (into {}))
+            ;; _               (pp/pprint probe-values)
+            ;; _               (println "apply just ran")
+            new-center      (key (apply max-key val probe-values))]
+        ;; (println "inside let")
+        ;; (println new-center)
+        (if (> 19 probe-round)
+          (recur new-center (quot dimension 2) (inc probe-round)))))))
+
+(comment
+  (def probe-values '({[256 256] 36} {[128 128] 21} {[384 128] 3} {[384 384] 19} {[128 384] 159}))
+  (pp/pprint probe-values)
+  (def pv-map (into {} probe-values))
+  (apply max-key val [95 477])
+  (apply max-key val pv-map)
+  )
 
 (defn -getName
   "Return the name of your bot."
@@ -17,7 +57,4 @@
    After returning from this method, your score for the plot will be the value of the largest
    query you made from the plot."
   [this probe]
-  (dac/divide-and-conquer [256 256] 512 probe)
-  ;; (comment Query for the value of a coordinate.)
-  ;; (.query probe (new xyz.jmullin.prospector.game.Coord 123 123))
-  )
+  (divide-and-conquer [256 256] 512 probe))
